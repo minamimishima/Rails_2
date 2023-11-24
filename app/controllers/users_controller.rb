25dc5acfe5abf944f13e3769e722871b01322045
minamimishima <146907532+minamimishima@users.noreply.github.com>
2023-11-24 11:52:54 +0900

    Userコントローラーの機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def index
+    @user = current_user
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def account_show
+    @user = current_user
+  end
+
+  def profile_show
+    @user = current_user
+  end
+
+  def profile_edit
+    @user = current_user
+  end
+
+  def profile_update
+    binding.pry
+    @user = current_user
+    if @user.update(profile_params)
+      redirect_to profile_path
+    else
+      render "profile_edit"
+    end
+  end
+
+  def destroy
+  end
+
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:profile_update, keys: [:name, :image, :introduction])
+  end
+
+  def profile_params
+    params.require(:user).permit(:name, :image, :introduction)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root to: "users#index"
+
+  get "account", to: "users#account_show"
+
+  get "profile", to: "users#profile_show"
+  get "profile/edit", to: "users#profile_edit"
+  post "profile/edit", to: "users#profile_update"
+
+  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
